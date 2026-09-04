72a2f5763c1cadd840590f6b5db91ca427f3f73a
Ruh <ruh@tut.by>
2026-09-04 10:12:31 +0300

    init: added base models

---
+++ src/OrganisationBranchesWeb/OrganisationBranchesWeb.Domain/Model/Cash.cs
@@ -0,0 +1,25 @@
+﻿namespace OrganisationBranchesWeb.Domain.Model
+{
+	public class Cash
+	{
+		/// <summary>
+		/// Cash Id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Cash name.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Department identifier.
+		/// </summary>
+		public int DepartmentId { get; set; }
+
+		/// <summary>
+		/// Navigate to department.
+		/// </summary>
+		public Department Department { get; set; }
+	}
+}
---
+++ src/OrganisationBranchesWeb/OrganisationBranchesWeb.Domain/Model/Department.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Principal;
+
+namespace OrganisationBranchesWeb.Domain.Model
+{
+	public class Department
+	{
+		/// <summary>
+		/// Department id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Department name.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Department address.
+		/// </summary>
+		public string Address { get; set; }
+
+		/// <summary>
+		/// Department code.
+		/// </summary>
+		public int Code { get; set; }
+
+		/// <summary>
+		/// Cash id.
+		/// </summary>
+		public int CashId { get; set; }
+
+		/// <summary>
+		/// Navigate to cashes.
+		/// </summary>
+		public ICollection<Cash> Cashes { get; set; }
+	}
+}
